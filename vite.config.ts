ce61969208c02ab1389da0b2a96279c70529fffb
Aser Nasr <asernasr@figranium.dev>
2026-09-09 14:05:16 -0400

    Add library build configuration

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@figranium-source': fileURLToPath(new URL('./.figranium-source/src', import.meta.url)),
+    },
+  },
+  build: {
+    lib: {
+      entry: fileURLToPath(new URL('./src/index.ts', import.meta.url)),
+      formats: ['es'],
+      fileName: 'index',
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom', 'react/jsx-runtime'],
+    },
+  },
+});
